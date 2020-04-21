b8009f3adc63f067140b3a02f0de386c7798bba6
picpoint <picpoint@yandex.ru>
2020-04-21 16:45:55 +0300

    start write js script

---
+++ src/js/meatsParts.js
@@ -0,0 +1,19 @@
+const partrib = document.querySelector('.cstk__partrib');
+const partchuck = document.querySelector('.cstk__partchuck');
+const partloin = document.querySelector('.cstk__partloin');
+const partsirloin = document.querySelector('.cstk__partsirloin');
+const partround = document.querySelector('.cstk__partround');
+const partflank = document.querySelector('.cstk__partflank');
+const partshortplate = document.querySelector('.cstk__partshortplate');
+const partforeshank = document.querySelector('.cstk__partforeshank');
+const partbrisket = document.querySelector('.cstk__partbrisket');
+const front = document.querySelector('.cstk__front');
+const back = document.querySelector('.cstk__back');
+
+
+
+
+
+partrib.addEventListener('click', () => {
+	
+});
