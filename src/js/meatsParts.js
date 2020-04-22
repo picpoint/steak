33dd9affaa06ea02599291405d784f66761ac014
picpoint <picpoint@yandex.ru>
2020-04-22 12:21:26 +0300

    start write class

--- src/js/meatsParts.js
+++ src/js/meatsParts.js
@@ -25,6 +25,25 @@ class choosePart {
 }
 
 
+// let rib = document.querySelector('.cstk__partrib');
+// const partchuck = document.querySelector('.cstk__partchuck');
+// const partloin = document.querySelector('.cstk__partloin');
+// const partsirloin = document.querySelector('.cstk__partsirloin');
+// const partround = document.querySelector('.cstk__partround');
+// const partflank = document.querySelector('.cstk__partflank');
+// const partshortplate = document.querySelector('.cstk__partshortplate');
+// const partforeshank = document.querySelector('.cstk__partforeshank');
+// const partbrisket = document.querySelector('.cstk__partbrisket');
+
+
+
+
+
+
+
+
+
+
 
 partrib.addEventListener('click', (e) => {
   let elemfront = front.firstElementChild;
